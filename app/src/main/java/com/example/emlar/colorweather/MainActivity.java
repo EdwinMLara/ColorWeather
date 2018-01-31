57eb06efdf5b0f3b7496161144c7ab653b0ce7a8
Edwin Miguel Lara Espinoza <em.laraespinoza@hotmail.com>
2018-01-30 23:09:51 -0600

    agregando la libreria butterknife y abriendo activitys con intents

--- app/src/main/java/com/example/emlar/colorweather/MainActivity.java
+++ app/src/main/java/com/example/emlar/colorweather/MainActivity.java
@@ -1,13 +1,45 @@
 package com.example.emlar.colorweather;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.DailyWeatherTextView) TextView DailyWeatherTextView;
+    @BindView(R.id.HourlyWeatherTextView) TextView HourlyWeatherTextView;
+    @BindView(R.id.MinutelyWeatherTextView) TextView MinutelyWeatherTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+    }
+
+    @OnClick(R.id.DailyWeatherTextView)
+    public void DailyWeatherClick(){
+        Intent DailyWeatherIntend = new Intent(MainActivity.this,DailyWeatherActivity.class);
+        startActivity(DailyWeatherIntend);
+    }
+
+    @OnClick(R.id.HourlyWeatherTextView)
+    public void HourlyWeatherClick(){
+        Intent HourlyWeatherIntend = new Intent(MainActivity.this,HourlyWatherActivity.class);
+        startActivity(HourlyWeatherIntend);
+    }
+
+    @OnClick(R.id.MinutelyWeatherTextView)
+    public void MinutelyWeatherClick(){
+        Intent MinutelyWeatherIntent = new Intent(MainActivity.this,MinutelyWeatherActivity.class);
+        startActivity(MinutelyWeatherIntent);
     }
 }
