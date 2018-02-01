67226bf391a01a1ad7ca986c1352a47afdafc714
Edwin Miguel Lara Espinoza <em.laraespinoza@hotmail.com>
2018-01-31 23:29:57 -0600

    haciendo la primer peticion a servicio web google  con la libreria volley y agregando permisos de internet y el estado de la red al android manifest

--- app/src/main/java/com/example/emlar/colorweather/MainActivity.java
+++ app/src/main/java/com/example/emlar/colorweather/MainActivity.java
@@ -3,8 +3,16 @@ package com.example.emlar.colorweather;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -12,6 +20,7 @@ import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
+    public final static String TAG = MainActivity.class.getSimpleName();
 
     @BindView(R.id.DailyWeatherTextView) TextView DailyWeatherTextView;
     @BindView(R.id.HourlyWeatherTextView) TextView HourlyWeatherTextView;
@@ -29,6 +38,27 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         CurrentWeather currentWeather = new CurrentWeather(MainActivity.this);
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://www.google.com";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        Log.d(TAG,"Response is: "+response.substring(0,5000));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d(TAG,"that didnt work: ");
+            }
+        });
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
 
     }
 
